27b3dca4c6f632d8278a73f0b5f7d597e828c36a
iWantToBeAProgrammer <juanstevenson10@gmail.com>
2022-10-13 00:42:37 +0700

    Text effect etc

--- dist/js/script.js
+++ dist/js/script.js
@@ -2,14 +2,61 @@
 window.onscroll = function() {
     const header = document.querySelector ('header');
     const fixedNav = header.offsetTop;
+    const toTop = document.querySelector ('#to-top')
 
     if(window.pageYOffset > fixedNav) {
         header.classList.add('navbar-fixed');
+        toTop.classList.remove('hidden')
+        toTop.classList.add('flex')
     } else {
         header.classList.remove('navbar-fixed');
+        toTop.classList.remove('flex')
+        toTop.classList.add('hidden')
     }
 }
 
+// typing effects
+const typedTextSpan = document.querySelector(".typed-text");
+const cursorSpan = document.querySelector(".cursor");
+
+const textArray = ["Student", "Programmer"];
+const typingDelay = 200;
+const erasingDelay = 100;
+const newTextDelay = 2000; // Delay between current and next text
+let textArrayIndex = 0;
+let charIndex = 0;
+
+function type() {
+  if (charIndex < textArray[textArrayIndex].length) {
+    if(!cursorSpan.classList.contains("typing")) cursorSpan.classList.add("typing");
+    typedTextSpan.textContent += textArray[textArrayIndex].charAt(charIndex);
+    charIndex++;
+    setTimeout(type, typingDelay);
+  } 
+  else {
+    cursorSpan.classList.remove("typing");
+  	setTimeout(erase, newTextDelay);
+  }
+}
+
+function erase() {
+	if (charIndex > 0) {
+    if(!cursorSpan.classList.contains("typing")) cursorSpan.classList.add("typing");
+    typedTextSpan.textContent = textArray[textArrayIndex].substring(0, charIndex-1);
+    charIndex--;
+    setTimeout(erase, erasingDelay);
+  } 
+  else {
+    cursorSpan.classList.remove("typing");
+    textArrayIndex++;
+    if(textArrayIndex>=textArray.length) textArrayIndex=0;
+    setTimeout(type, typingDelay + 1100);
+  }
+}
+
+document.addEventListener("DOMContentLoaded", function() { // On DOM Load initiate the effect
+  if(textArray.length) setTimeout(type, newTextDelay + 250);
+});
 // Hamburger
 const hamburger = document.querySelector ('#hamburger');
 const navMenu = document.querySelector ('#nav-menu')
@@ -19,3 +66,11 @@ hamburger.addEventListener('click', function() {
     hamburger.classList.toggle('hamburger-active');
     navMenu.classList.toggle('hidden');
 })
+
+//klik diluar hamburger
+window.addEventListener('click', function (e) {
+    if(e.target != hamburger && e.target != navMenu) {
+        hamburger.classList.remove('hamburger-active')
+        navMenu.classList.add('hidden')
+    }
+ })
